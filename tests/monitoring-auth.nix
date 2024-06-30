7f6b756b92e559acb0c26e11946971e9937b9c3e
Peter Esselius <peter.esselius@pagero.com>
2024-06-30 08:02:31 +0200

    clean up monitoring-auth test

--- tests/monitoring-auth.nix
+++ tests/monitoring-auth.nix
@@ -1,91 +1,89 @@
-{ myModules, inputs, config, ... }:
+{ myModules, inputs, ... }:
 
 {
-  name = "grafana-auth";
+  name = "monitoring-auth";
 
-  extraPythonPackages = p: [ p.playwright ];
+  nodes.machine = { modulesPath, ... }: {
+    _module.args.mkAuthentikScope = inputs.authentik-nix.lib.mkAuthentikScope;
 
-  nodes = {
-    machine = { modulesPath, ... }: {
-      _module.args.mkAuthentikScope = inputs.authentik-nix.lib.mkAuthentikScope;
+    virtualisation = {
+      memorySize = 2048;
+    };
 
-      virtualisation = {
-        memorySize = 2048;
-      };
+    imports = [
+      inputs.authentik-nix.nixosModules.default
 
-      imports = [
-        inputs.authentik-nix.nixosModules.default
+      myModules.auth
+      myModules.monitoring
+    ];
 
-        myModules.auth
-        myModules.monitoring
-      ];
+    networking.firewall.enable = false;
 
-      networking.firewall.enable = false;
+    auth = {
+      enable = true;
+      env-file = builtins.toFile "authentik-env-file" ''
+        AUTHENTIK_SECRET_KEY=qwerty123456
+        AUTHENTIK_BOOTSTRAP_PASSWORD=password
+        AUTHENTIK_BOOTSTRAP_TOKEN=token
+      '';
+      blueprints = [{
+        metadata.name = "grafana-oauth";
+        entries = [
+          {
+            model = "authentik_providers_oauth2.oauth2provider";
+            state = "present";
+            identifiers.name = "Grafana";
+            id = "provider";
+            attrs = {
+              authentication_flow = "!Find [authentik_flows.flow, [slug, default-authentication-flow]]";
+              authorization_flow = "!Find [authentik_flows.flow, [slug, default-provider-authorization-explicit-consent]]";
+              client_type = "confidential";
+              client_id = "grafana";
+              client_secret = "secret";
+              access_code_validity = "minutes=1";
+              access_token_validity = "minutes=5";
+              refresh_token_validity = "days=30";
+              property_mappings = [
+                "!Find [authentik_providers_oauth2.scopemapping, [scope_name, openid]]"
+                "!Find [authentik_providers_oauth2.scopemapping, [scope_name, email]]"
+                "!Find [authentik_providers_oauth2.scopemapping, [scope_name, profile]]"
+                "!Find [authentik_providers_oauth2.scopemapping, [scope_name, offline_access]]"
+              ];
+              sub_mode = "hashed_user_id";
+              include_claims_in_id_token = true;
+              issuer_mode = "per_provider";
+            };
+          }
+          {
+            model = "authentik_core.application";
+            state = "present";
+            identifiers.slug = "grafana";
+            id = "grafana";
+            attrs = {
+              name = "Grafana";
+              provider = "!KeyOf provider";
+              policy_engine_mode = "any";
+            };
+          }
+        ];
+      }];
+    };
 
-      auth = {
-        enable = true;
-        env-file = builtins.toFile "authentik-env-file" ''
-          AUTHENTIK_SECRET_KEY=qwerty123456
-          AUTHENTIK_BOOTSTRAP_PASSWORD=password
-          AUTHENTIK_BOOTSTRAP_TOKEN=token
-        '';
-        blueprints = [{
-          metadata.name = "grafana-oauth";
-          entries = [
-            {
-              model = "authentik_providers_oauth2.oauth2provider";
-              state = "present";
-              identifiers.name = "Grafana";
-              id = "provider";
-              attrs = {
-                authentication_flow = "!Find [authentik_flows.flow, [slug, default-authentication-flow]]";
-                authorization_flow = "!Find [authentik_flows.flow, [slug, default-provider-authorization-explicit-consent]]";
-                client_type = "confidential";
-                client_id = "grafana";
-                client_secret = "secret";
-                access_code_validity = "minutes=1";
-                access_token_validity = "minutes=5";
-                refresh_token_validity = "days=30";
-                property_mappings = [
-                  "!Find [authentik_providers_oauth2.scopemapping, [scope_name, openid]]"
-                  "!Find [authentik_providers_oauth2.scopemapping, [scope_name, email]]"
-                  "!Find [authentik_providers_oauth2.scopemapping, [scope_name, profile]]"
-                  "!Find [authentik_providers_oauth2.scopemapping, [scope_name, offline_access]]"
-                ];
-                sub_mode = "hashed_user_id";
-                include_claims_in_id_token = true;
-                issuer_mode = "per_provider";
-              };
-            }
-            {
-              model = "authentik_core.application";
-              state = "present";
-              identifiers.slug = "grafana";
-              id = "grafana";
-              attrs = {
-                name = "Grafana";
-                provider = "!KeyOf provider";
-                policy_engine_mode = "any";
-              };
-            }
-          ];
-        }];
-      };
-
-      monitoring = {
-        enable = true;
-        grafana = {
-          oauth = {
-            client_id_file = builtins.toFile "grafana-client-id" "grafana";
-            client_secret_file = builtins.toFile "grafana-client-secret" "secret";
-            auth_url = "http://127.0.0.1:9000/application/o/authorize/";
-            token_url = "http://127.0.0.1:9000/application/o/token/";
-            api_url = "http://127.0.0.1:9000/application/o/userinfo/";
-          };
+    monitoring = {
+      enable = true;
+      grafana = {
+        oauth = {
+          client_id_file = builtins.toFile "grafana-client-id" "grafana";
+          client_secret_file = builtins.toFile "grafana-client-secret" "secret";
+          auth_url = "http://127.0.0.1:9000/application/o/authorize/";
+          token_url = "http://127.0.0.1:9000/application/o/token/";
+          api_url = "http://127.0.0.1:9000/application/o/userinfo/";
         };
       };
     };
   };
+
+  extraPythonPackages = p: [ p.playwright ];
 
   testScript = ''
     start_all()
